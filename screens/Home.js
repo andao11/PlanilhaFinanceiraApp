01aaa1143ec43de1c6ea36145e32abe64ab17b1a
Anderson Francisco <andersonfrancisco@Doctor-Web-Execucao.local>
2018-01-17 16:49:47 -0200

    Home fix

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import {Header} from 'react-native-elements';
+import { Header } from 'react-native-elements';
 
 export default class Home extends Component {
 
     render(){
         return(
             <Header
-                leftComponent={{ icon: 'menu', color: '#fff' }}
                 centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
             />
         )
     }
